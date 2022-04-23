e8a2703da660d365bbe423eec105f2df28adb724
ArnoldVanN <90195380+ArnoldVanN@users.noreply.github.com>
2022-04-23 02:44:43 +0200

    Update simpleServer.sh

--- simpleServer.sh
+++ simpleServer.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+echo "installing lastest java"
+dnf install java-latest-openjdk
+echo "installing screen"
+dnf install screen 
 
+mkdir minecraft
+
+cd minecraft
+echo "downloading minecraft server.jar"
+wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+
+mv server.jar minecraft-server-1.18.2.jar
+echo "initializing server for the first time"
+java -jar minecraft-server-1.18.2.jar
